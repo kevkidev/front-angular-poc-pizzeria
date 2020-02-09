abcc3b1c831e417e366dcf8af5ecf52eb30777c7
Ketwenty <>
2020-02-09 19:51:32 +0100

    Base reactive Form

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -8,11 +8,18 @@ import { FormControl } from '@angular/forms';
 })
 export class UserComponent implements OnInit {
 
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  })
+
   userInput = "hello";
   form1 = new FormControl('Kevin');
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  showForm() {
+    console.log(this.userForm);
+  }
 }
